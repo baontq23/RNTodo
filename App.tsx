131f57dba8f8888e7f99580d5a4ad68c8cf85f37
baontq23 <baooshacker@gmail.com>
2022-09-02 13:57:34 +0700

    refactor: clean unused status bar config

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import AppContainer from './src/components/app-container';
 import Navigator from './src';
-import { StatusBar } from 'native-base';
 
 const App = () => {
   return (
     <AppContainer>
-      <StatusBar barStyle='light-content' />
       <Navigator />
     </AppContainer>
   );
